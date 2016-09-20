b0406a1b3bca6311cab14d7c87560dd33f9ad350
ssigg <stephan.sigg@gmail.com>
2016-09-20 21:10:28 +0200

    Use Gulp for deployment

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require('gulp');
+
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cleanCss = require('gulp-clean-css');
+var replace = require('gulp-replace');
+
+var bases = {
+    customer: 'dist/customer/'
+};
+
+var paths = {
+    stylesheets: [
+        'app/bower_components/skeleton-css/css/normalize.css',
+        'app/bower_components/skeleton-css/css/skeleton.css'
+    ],
+    config: [
+        'app/config/*.js'
+    ],
+    scripts: [
+        'app/bower_components/angular/angular.js',
+        'app/bower_components/angular-resource/angular-resource.js',
+        'app/bower_components/angular-route/angular-route.js',
+        'app/bower_components/ocanvas/index.js',
+        'app/bower_components/underscore/underscore.js',
+        'app/components/*/*.js',
+        '!app/components/*/*_test.js',
+        '!app/config/*.js'],
+    templates: [
+        'app/components/*/*.html'
+    ],
+    customer: {
+        stylesheets: [
+            'app/customer/app.css'
+        ],
+        page: [
+            'app/customer/index.html'
+        ],
+        scripts: [
+            'app/customer/app.js',
+            'app/customer/*/*.js',
+            '!app/customer/*/*_test.js' 
+        ],
+        templates: [
+            'app/customer/*/*.html'
+        ]
+    }
+};
+
+gulp.task('customer-clean', function() {
+    return gulp.src(bases.customer)
+        .pipe(clean());
+});
+
+gulp.task('customer-config', [ 'customer-clean' ], function() {
+    return gulp.src(paths.config)
+        .pipe(gulp.dest(bases.customer));
+});
+
+gulp.task('customer-scripts', [ 'customer-clean' ], function() {
+    return gulp.src(paths.scripts.concat(paths.customer.scripts))
+        .pipe(uglify({ mangle: false }))
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest(bases.customer));
+});
+
+gulp.task('customer-stylesheets', [ 'customer-clean' ], function() {
+    return gulp.src(paths.stylesheets.concat(paths.customer.stylesheets))
+        .pipe(cleanCss())
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest(bases.customer));
+});
+
+gulp.task('customer-templates', [ 'customer-clean' ], function() {
+    return gulp.src(paths.templates.concat(paths.customer.templates), { base: './app/customer/' })
+        .pipe(gulp.dest(bases.customer));
+});
+
+gulp.task('customer-page', [ 'customer-clean' ], function() {
+    return gulp.src(paths.customer.page)
+        .pipe(replace(/<!-- stylesheets -->([\w\W\s]*)<!-- \/stylesheets -->/, '<link rel="stylesheet" href="app.min.css">'))
+        .pipe(replace(/<!-- scripts -->([\w\W\s]*)<!-- \/scripts -->/, '<script src="app.min.js"></script>\n<script src="config.js"></script>'))
+        .pipe(gulp.dest(bases.customer));
+});
+
+gulp.task('customer', [ 'customer-config', 'customer-scripts', 'customer-stylesheets', 'customer-templates', 'customer-page' ]);
