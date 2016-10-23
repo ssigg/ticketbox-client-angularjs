e7499593ec1f7b45701e0760040945a90d1ccf0e
ssigg <stephan.sigg@gmail.com>
2016-10-23 17:15:52 +0200

    Move seatplan style configuration to config

--- app/customer.config/config_sample.js
+++ app/customer.config/config_sample.js
@@ -4,4 +4,13 @@ angular.module('ticketbox.config', [])
 
     .constant('api', '<ticketbox-server-url>')
     
-    .constant('currency', '$');
+    .constant('currency', '$')
+    
+    .factory('styles', function() {
+        return {
+            'free': { 'background': '#3f3', 'stroke': '1px solid #000', 'opacity': 0.2 },
+            'freeHover': { 'background': '#282', 'stroke': '1px solid #000', 'opacity': 0.5 },
+            'reserved': { 'background': '#000', 'stroke': '1px solid #000', 'opacity': 1 },
+            'reservedbymyself': { 'background': '#f33', 'stroke': '1px solid #000', 'opacity': 0.4 }
+        };
+    });
