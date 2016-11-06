370990eebe4a7478ce9dd62f62bbb16496e6ce92
ssigg <stephan.sigg@gmail.com>
2016-11-06 12:06:50 +0100

    Update admin sample config

--- app/admin.config/config_sample.js
+++ app/admin.config/config_sample.js
@@ -4,4 +4,12 @@ angular.module('ticketbox.config', [])
 
     .constant('api', '<ticketbox-server-url>')
     
-    .constant('currency', '$');
+    .constant('currency', '$')
+    
+    .factory('styles', function() {
+        return {
+            'free': { 'background': '#fff', 'stroke': '0px solid #000', 'opacity': 0 },
+            'ordered': { 'background': '#16f', 'stroke': '1px solid #000', 'opacity': 0.5 },
+            'sold': { 'background': '#f33', 'stroke': '1px solid #000', 'opacity': 1 }
+        };
+    });
