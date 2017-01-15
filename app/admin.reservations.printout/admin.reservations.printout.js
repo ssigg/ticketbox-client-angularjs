a7ebd371085327e2f728449c1c2881428cd0400c
ssigg <stephan.sigg@gmail.com>
2017-01-15 19:08:05 +0100

    List customer purchases in admin interface

--- app/admin.reservations.printout/admin.reservations.printout.js
+++ app/admin.reservations.printout/admin.reservations.printout.js
@@ -11,9 +11,10 @@ angular.module('ticketbox.admin.reservations.printout', [
         });
     })
 
-    .controller('ReservationsPrintoutCtrl', function($scope, $routeParams, $q,  Reservation, Event, Order, BoxofficePurchase, Seat, currency) {
+    .controller('ReservationsPrintoutCtrl', function($scope, $routeParams, $q,  Reservation, Event, Order, BoxofficePurchase, CustomerPurchase, Seat, currency) {
         $scope.currency = currency;
         $scope.event = Event.get({ id: $routeParams.id });
         $scope.orders = Order.query({ event_id: $routeParams.id });
         $scope.boxofficePurchases = BoxofficePurchase.query({ event_id: $routeParams.id });
+        $scope.customerPurchases = CustomerPurchase.query({ event_id: $routeParams.id });
     })
